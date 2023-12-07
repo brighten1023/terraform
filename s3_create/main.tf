bb93442c6c22e78a851c9e8d03c600ab34a3388c
bright <brighten1023@gmail.com>
2023-12-07 16:56:26 -0500

    add more

--- s3_create/main.tf
+++ s3_create/main.tf
@@ -22,7 +22,7 @@ module "s3_storage" {
 
 module "upload" {
   source = "./modules/upload_image"
-  bucket_name = "project_s3_storage"
+  bucket_name = module.s3_storage.bucket_id
   file_name = "aws_picture.png"
   file_path = "./aws_picture.png"
 }
